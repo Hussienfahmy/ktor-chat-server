f36130747e013fc9bde38979094301c8accbc1de
Hussien <hussien.fahmy98@gmail.com>
2023-10-03 21:55:24 +0300

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.h_fahmy.chat.ktor-chat"
---
+++ src/main/kotlin/com/h_fahmy/chat/Application.kt
@@ -0,0 +1,19 @@
+package com.h_fahmy.chat
+
+import com.h_fahmy.chat.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSockets()
+    configureSerialization()
+    configureMonitoring()
+    configureSecurity()
+    configureRouting()
+}
