e3c5de84ab472dec576c0546f6d349aba0f8d1bb
Hussien <hussien.fahmy98@gmail.com>
2023-10-06 10:19:11 +0300

    Added ChatSession

--- src/main/kotlin/com/h_fahmy/chat/plugins/Security.kt
+++ src/main/kotlin/com/h_fahmy/chat/plugins/Security.kt
@@ -1,22 +1,26 @@
 package com.h_fahmy.chat.plugins
 
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import io.ktor.server.sessions.*
+import com.h_fahmy.chat.sessions.ChatSession
+import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationCallPipeline.ApplicationPhase.Plugins
+import io.ktor.server.application.call
+import io.ktor.server.application.install
+import io.ktor.server.sessions.Sessions
+import io.ktor.server.sessions.cookie
+import io.ktor.server.sessions.get
+import io.ktor.server.sessions.sessions
+import io.ktor.server.sessions.set
+import io.ktor.util.generateNonce
 
 fun Application.configureSecurity() {
-    data class MySession(val count: Int = 0)
     install(Sessions) {
-        cookie<MySession>("MY_SESSION") {
-            cookie.extensions["SameSite"] = "lax"
-        }
+        cookie<ChatSession>("SESSION")
     }
-    routing {
-        get("/session/increment") {
-                val session = call.sessions.get<MySession>() ?: MySession()
-                call.sessions.set(session.copy(count = session.count + 1))
-                call.respondText("Counter is ${session.count}. Refresh to increment.")
-            }
+
+    intercept(Plugins) {
+        if (call.sessions.get<ChatSession>() == null) {
+            val userName = call.parameters["username"]?: "Guest"
+            call.sessions.set(ChatSession(userName=userName, sessionId = generateNonce()))
+        }
     }
 }
---
+++ src/main/kotlin/com/h_fahmy/chat/sessions/ChatSession.kt
@@ -0,0 +1,6 @@
+package com.h_fahmy.chat.sessions
+
+data class ChatSession(
+    val userName:String,
+    val sessionId:String
+)
